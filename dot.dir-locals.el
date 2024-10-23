971a4bdb42b7d40a77f3d82382887a81349f9407
yjtknst <yuji.takenoshita@gmail.com>
2024-10-23 17:59:50 +0900

    Support for use as a submodule

--- dot.dir-locals.el
+++ dot.dir-locals.el
@@ -1,5 +1,5 @@
 ((org-mode
   (eval . (setq org-export-async-init-file
-                (expand-file-name "org-export-async-init.el" (file-name-directory (or load-file-name buffer-file-name)))))
+                (expand-file-name "config/org-export-async-init.el" (file-name-directory (or load-file-name buffer-file-name)))))
   (eval . (setq org-export-in-background t))))
 
