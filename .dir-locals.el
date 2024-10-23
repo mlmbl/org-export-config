eb693c12281550c7440e24b3dbf11f393a090def
yjtknst <yuji.takenoshita@gmail.com>
2024-10-23 10:28:17 +0900

    bibtex生成を外部パッケージにした

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,8 +1,5 @@
 ((org-mode
-  (eval . (let ((project-dir (file-name-directory (or load-file-name buffer-file-name default-directory))))
-            (when project-dir
-              ;; lisp ディレクトリ内のすべての .el または .elc ファイルをロード
-              (dolist (file (directory-files (expand-file-name "lisp" project-dir) t "\\.el\\(c\\)?$"))
-                (load file)))))
   (eval . (setq org-export-async-init-file
-                (expand-file-name "org-export-async-init.el" (file-name-directory (or load-file-name buffer-file-name)))))))
+                (expand-file-name "org-export-async-init.el" (file-name-directory (or load-file-name buffer-file-name)))))
+  (eval . (setq org-export-in-background t))))
+
